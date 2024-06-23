b7f003bf4363617c395549db631f0de442442509
NE-KK <nils.eckmann@mein.gmx>
2024-06-23 12:53:14 +0200

    First playable

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
 
 searched_number = random.randint(1, 100)
+is_won = False
 
-print("Hello")
-print("Bye")
 print(searched_number)
 
-guessed_number = int(input("Guess a number between 1 and 100: "))
-print("Hello again")
+
+while not is_won:
+    guessed_number = int(input("Guess a number between 1 and 100: "))
+
+    if guessed_number > searched_number:
+        print(f"{guessed_number} is to high!")
+    elif guessed_number < searched_number:
+        print(f"{guessed_number} is to low!")
+    else:
+        print(f"Congratulations. {guessed_number} is correct.")
+        is_won = True
+
+print("END")
