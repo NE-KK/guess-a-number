02ddff6a0c4eaaf645186ef8e54c3173315c4ca2
NE-KK <nils.eckmann@mein.gmx>
2024-06-27 21:02:41 +0200

    First playable

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
 
 searched_number = random.randint(1, 100)
+attempts = 0
 is_won = False
 
 print(searched_number)
 
 
 while not is_won:
+    attempts += 1
     guessed_number = int(input("Guess a number between 1 and 100: "))
 
     if guessed_number > searched_number:
@@ -15,6 +17,7 @@ while not is_won:
         print(f"{guessed_number} is to low!")
     else:
         print(f"Congratulations. {guessed_number} is correct.")
+        print(f"You finished in {attempts} attempts.")
         is_won = True
 
 print("END")
